cb462bd80b2ae3ec5657fbab757627c53bbb3571
Ilya <36731372+LinderJK@users.noreply.github.com>
2024-03-08 20:17:03 +0300

    chore: RSS-PZ-34 install dependency (#35)
    * feat: npm install, add webpack, typeScript, prettier settings
    
    * feat: add husky pre-commit, pre-push
    
    * feat: add index.html, index.ts, style.scss
    
    * fix: ci:format script
    
    * feat: add app.ts file
    
    * fix: eslint config
    
    * fix: validate-branch-name pattern
    
    * fix: pre-push husky run

---
+++ rss-puzzle/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const currentLoader = process.env.NODE_ENV === 'production' ? MiniCssExtractPlugin.loader : 'style-loader';
+
+module.exports = {
+    entry: path.resolve(__dirname, './src/index.ts'),
+    output: {
+        clean: true,
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.(c|sa|sc)ss$/i,
+                use: [currentLoader, 'css-loader', 'sass-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+
+    plugins: [
+        new ESLintPlugin({ extensions: 'ts' }),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'style.css',
+        }),
+    ],
+};
